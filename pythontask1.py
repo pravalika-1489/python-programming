c309454c5779ff656e53b439d72820e63bd4c600
pravalika-1489 <56461959+pravalika-1489@users.noreply.github.com>
2019-10-22 11:51:46 +0530

    Add files via upload

---
+++ pythontask1.py
@@ -0,0 +1,5 @@
+'''python program which accepts the radius of a circle from the user and compute the area'''
+from math import pi
+radius=float(input("enetr the radius of the circle: "))
+print("the area of the circle with radius "+str(radius)+ " is: "+str(pi*radius**2))
+
